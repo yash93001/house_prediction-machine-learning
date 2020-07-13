e98261950d26495e044e27c2facff98137a8a931
yash93001 <67060865+yash93001@users.noreply.github.com>
2020-07-14 04:31:33 +0530

    Update project 1.py

--- project 1.py
+++ project 1.py
@@ -40,7 +40,7 @@ def performance_metric(y_true,y_predict):
 
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(features,prices,test_size =0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(features,prices,test_size =0.2, random_state=10)
 print("Training and testing split was successful.")
 
 
